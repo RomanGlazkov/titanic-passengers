e4d1f7d3038c8d1070b607c4c37c3a62c15fcc7f
Roman Glazkov <roman.glazkov@altcraft.com>
2024-11-05 23:50:20 +0300

    Add eslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { main } from "./main";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { main } from './main';
 
 const root = createRoot(document.getElementById('root'));
 
